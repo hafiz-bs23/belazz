8f51c4e76556aff404cf79def82ab6d31da2f9d8
RAHMAHX1 <hafizur.rahman@abbvie.com>
2023-01-11 23:32:18 +0600

    Update: Icon updated. Added content for modal.

--- src/components/buli.vue
+++ src/components/buli.vue
@@ -76,13 +76,13 @@ export default {
       </div>
     </div>
     <div class="position-absolute bottom-0 end-0">
-      <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#AboutModal">
         <font-awesome-icon icon="fa-solid fa-circle-info" />
         আমাদের তথ্য
       </button>
     </div>
     <div class="position-absolute bottom-0 start-0">
-      <button type="button" class="btn btn-success d-block" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <button type="button" class="btn btn-success d-block" data-bs-toggle="modal" data-bs-target="#SourceModal">
         <font-awesome-icon icon="fa-solid fa-circle-question" />
         উৎস
       </button>
@@ -91,18 +91,42 @@ export default {
 
 
   <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="SourceModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-header mx-auto">
+          <h4 class="modal-title text-center" id="exampleModalLabel">তথ্য উৎস</h4>
         </div>
         <div class="modal-body">
-          ...
+          <img src="/src/assets/images/icons/data-network.png" class="rounded mx-auto d-block mb-3"  height="150" alt="Source">
+          <p>
+            বাংলা ভাষা ব্যবহার কারির তুলনায় অনলাইনে বাংলা ভাষার ডিজিটাল সম্পদ খুবই কম। ভাষা নিয়ে কাজ করা লোকে বাঙলা ভাষার কঠিন দিক গূলোকে আঙ্গুল দিয়ে দেখিয়ে দেন এবং নিজের আপন ভাষা থেকে দূরে থাকেন।
+            এতো কিছু এর পরও গুটিকয়েক লোকজন এখনো বিনা পারিশ্রমিকে নিজের ভাষা এর জন্য কাজ করে যাচ্ছেন। নিজেদের উদ্যোগে বাংলা ভাষা এর ডিজিটাল সম্পপদ এর উন্নতি সাধন করছেন। এমন কিছু মানুষের সেই অমূল্য
+            সম্পদ এর সামান্য ই এখানে তুলে ধরা হয়েছে।
+          </p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">বন্ধ করুন</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="AboutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header mx-auto">
+          <h4 class="modal-title text-center" id="exampleModalLabel">আমাদের তথ্য</h4>
+        </div>
+        <div class="modal-body mx-auto">
+          <img src="/src/assets/images/icons/card.png" class="rounded mx-auto d-block mb-3"  height="150" alt="About">
+          <p><b>আমিঃ </b>মোঃ হাফিজুর রহমান</p>
+          <p><b>একজনঃ </b>সফটওয়্যার ক্র্যাফটম্যান</p>
+          <p><b>এবংঃ </b>একজন গর্বিত <b>বাংলাদেশী</b></p>
+          <p><b>মেইল করুনঃ </b>mdrahmanamin@gmail.com</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">বন্ধ করুন</button>
         </div>
       </div>
     </div>
--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -2,13 +2,13 @@
   <nav class="navbar bg-transparent">
     <div class="container-fluid">
       <a class="navbar-brand d-md-none" href="/">
-        <img src="/src/assets/logo.png" alt="" width="75"  class="d-inline-block align-text-top">
+        <img src="/src/assets/logov5.png" alt="" width="150"  class="d-inline-block align-text-top">
       </a>
       <a class="navbar-brand d-none d-md-block d-lg-none " href="/">
-        <img src="/src/assets/logo.png" alt="" width="150"  class="d-inline-block align-text-top">
+        <img src="/src/assets/logov5.png" alt="" width="200"  class="d-inline-block align-text-top">
       </a>
       <a class="navbar-brand d-none d-lg-block " href="/">
-        <img src="/src/assets/logo.png" alt="" width="200"  class="d-inline-block align-text-top">
+        <img src="/src/assets/logov5.png" alt="" width="250"  class="d-inline-block align-text-top">
       </a>
     </div>
   </nav>
